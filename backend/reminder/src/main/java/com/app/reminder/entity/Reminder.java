392e01eae5b2c0f5873348e027b78c632a18ca85
D4GUR4SU <douglas_search@hotmail.com>
2024-04-16 17:29:06 -0300

    Using annotation spring @Id

--- backend/reminder/src/main/java/com/app/reminder/entity/Reminder.java
+++ backend/reminder/src/main/java/com/app/reminder/entity/Reminder.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.bson.codecs.pojo.annotations.BsonId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -20,7 +20,7 @@ public class Reminder implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
-  @BsonId
+  @Id
   private String id;
   private String name;
   private String description;
