1293b7a2192ff316d703820e2e18f1c5235da623
D4GUR4SU <douglas_search@hotmail.com>
2024-04-18 00:25:48 -0300

    Authentication config

--- backend/authentication/src/main/java/com/reminder/authentication/config/AppConfig.java
+++ backend/authentication/src/main/java/com/reminder/authentication/config/AppConfig.java
@@ -1,34 +1,40 @@
 package com.reminder.authentication.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@RefreshScope
 @Configuration
 public class AppConfig {
 
-  @Value("${jwt.secret}")
-  private String jwtSecret;
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    Logger log = LoggerFactory.getLogger(AppConfig.class);
 
-  @Bean
-  public JwtAccessTokenConverter accessTokenConverter() {
-    JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-    tokenConverter.setSigningKey(jwtSecret);
-    return tokenConverter;
-  }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        log.debug("AppConfig::bCryptPasswordEncoder");
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public JwtTokenStore tokenStore() {
-    return new JwtTokenStore(accessTokenConverter());
-  }
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        log.debug("AppConfig::accessTokenConverter");
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MTMyNzUzMDgsImV4cCI6MTc0NDgxMTMwOCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.vQhm1_oP3jQMQAuN9s-NlwZYWjvl9TSP9Ntd0zUtSFA");
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        log.debug("AppConfig::tokenStore");
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
 }
